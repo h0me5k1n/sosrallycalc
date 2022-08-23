b0b552ac363b1382e321d753d97229d6a92c956e
h0me5k1n <h0me5k1n@gmail.com>
2022-08-23 11:23:47 +0100

    uploaded from local development location

---
+++ format.py
@@ -0,0 +1,11 @@
+from os import system
+
+files = ["app.py", "keep_alive.py"]
+
+def format():
+    for i in files:
+        system(f"autopep8 --in-place --aggressive {i}")
+
+if __name__ == "__main__":
+    format()
+    print("All done!")
